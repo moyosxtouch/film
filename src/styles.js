0db0d933d5a58fb3452e826b020999b50b74fc61
moyo <moyosxtouch@gmail.com>
2025-02-05 13:36:57 -0600

    change in movie categories

--- src/styles.js
+++ src/styles.js
@@ -1,21 +1,21 @@
 import { makeStyles } from "@mui/styles";
-const drawerWidth = 5;
+const drawerWidth = 0;
 export default makeStyles((theme) => ({
-    root:{
-        display: 'flex',
-        height: '100%'
+  root: {
+    display: "flex",
+    height: "100%",
+  },
+  toolbar: {
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: 0,
+      width: "100%",
     },
-    toolbar:{
-        ...theme.mixins.toolbar,
-    },
-    content: {
-        flexGrow: 1,
-        padding:theme.spacing(3),
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        [theme.breakpoints.down('sm')]:{
-            marginLeft: 0,
-            width: '100%'
-        }
-    }
+  },
 }));
