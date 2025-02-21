2a88fb329ca8f21217ca574a8555188c8d850210
moyo <moyosxtouch@gmail.com>
2025-02-20 19:43:25 -0600

    end alan setup

--- src/Alan.jsx
+++ src/Alan.jsx
@@ -4,6 +4,10 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { ColorModeContext } from "./utils/ToggleColorMode";
 import { fetchToken } from "./utils";
+import {
+  searchMovie,
+  selectGenreOrCategory,
+} from "./features/currentGenreOrCategory";
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
   const dispatch = useDispatch();
@@ -13,8 +17,23 @@ const useAlan = () => {
     alanBtn({
       key: "f776b66f974e650989c3361596435e5c2e956eca572e1d8b807a3e2338fdd0dc/stage",
       host: "v1.alan.app/alanai",
-      onCommand: ({ command, mode }) => {
-        if (command === "changeMode") {
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        if (command === "chooseGenre") {
+          const foundGenre = genres.find(
+            (g) => g.name.toLowerCase() === genreOrCategory.toLowerCase()
+          );
+
+          if (foundGenre) {
+            navigate("/");
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            const category = genreOrCategory.startsWith("top")
+              ? "top_rated"
+              : genreOrCategory;
+            navigate("/");
+            dispatch(selectGenreOrCategory(category));
+          }
+        } else if (command === "changeMode") {
           if (mode === "light") {
             setMode("light");
           } else {
@@ -25,10 +44,12 @@ const useAlan = () => {
         } else if (command === "logout") {
           localStorage.clear();
           window.location.href = "/";
+        } else if (command === "search") {
+          dispatch(searchMovie(query));
         }
       },
     });
-  }, [setMode]);
+  }, [navigate, dispatch, setMode]);
 
   return null;
 };
